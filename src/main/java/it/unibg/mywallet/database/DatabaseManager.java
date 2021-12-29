2d10f4116f93c4795cf532df41138109d51b5478
andreatrionfetti <a.trionfetti@studenti.unibg.it>
2021-12-29 10:29:14 +0100

    Update DatabaseManager.java

--- src/main/java/it/unibg/mywallet/database/DatabaseManager.java
+++ src/main/java/it/unibg/mywallet/database/DatabaseManager.java
@@ -168,17 +168,18 @@ public class DatabaseManager {
 
 		int new_id;
 		ArrayList<Integer> ids = new ArrayList<Integer>();
-		String sql_select_azienda = "SELECT ID FROM PAGAMENTO";
-		String sql_select_persona = "SELECT ID FROM PRESTITO";
+		String sql_select_pagamento = "SELECT ID FROM PAGAMENTO";
+		String sql_select_prestito = "SELECT ID FROM PRESTITO";
+		String sql_update_bilancio = null;
 
 		Statement stmt = null;
 		
 		try(PreparedStatement insertPayment = this.conn.prepareStatement("INSERT INTO PAGAMENTO(ID, ID_utente, ammontare, data_contabilizzazione) VALUES(?,?,?,?)")) {
 			
 			stmt = this.conn.createStatement();
-			ResultSet rs_azienda = stmt.executeQuery(sql_select_azienda);
+			ResultSet rs_azienda = stmt.executeQuery(sql_select_pagamento);
 			while (rs_azienda.next()) ids.add(Integer.valueOf(rs_azienda.getInt("ID")));
-			ResultSet rs_persona = stmt.executeQuery(sql_select_persona);
+			ResultSet rs_persona = stmt.executeQuery(sql_select_prestito);
 			while (rs_persona.next()) ids.add(Integer.valueOf(rs_persona.getInt("ID")));
 			if(ids.isEmpty()) {
 				new_id = 0;
